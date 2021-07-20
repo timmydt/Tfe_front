17a69c8f516b0a14a3253f834d86325aa1a62b74
Timmy <timmy.detroch@outlook.com>
2021-07-20 17:08:25 +0200

    premier commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,58 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonButton,
+  IonContent,
+  IonHeader,
+  IonInput,
+  IonItem,
+  IonItemDivider,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { useState } from "react";
+import "./Home.css";
+import axios from "axios";
 
 const Home: React.FC = () => {
+  const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>();
+
+  async function login() {
+    console.log("je me log");
+    const data = await axios.post("http://localhost:4000/users/login", {
+      username: username,
+      password: password,
+    });
+    console.log(data);
+  }
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Login</IonTitle>
         </IonToolbar>
       </IonHeader>
+
       <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
+        <IonItemDivider>Connexion</IonItemDivider>
+        <IonItem>
+          <IonInput
+            value={username}
+            placeholder="Username"
+            onIonChange={(e) => setUsername(e.detail.value!)}
+          ></IonInput>
+        </IonItem>
+        <IonItem>
+          <IonInput
+            value={password}
+            type="password"
+            placeholder="Password"
+            onIonChange={(e) => setPassword(e.detail.value!)}
+          ></IonInput>
+        </IonItem>
+        <IonButton onClick={login}>Connect</IonButton>
+        <IonButton class="createaccountbutton"> Créer un compte </IonButton>
       </IonContent>
     </IonPage>
   );
