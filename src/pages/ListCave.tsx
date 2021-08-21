effa172b9f6cb928fdfafb3f268355658ce423f6
Timmy <timmy.detroch@outlook.com>
2021-08-22 00:03:14 +0200

    à fixer

--- src/pages/ListCave.tsx
+++ src/pages/ListCave.tsx
@@ -7,16 +7,34 @@ import {
   IonToolbar,
 } from "@ionic/react";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { axiosInstance } from "../helpers/axios";
 
 const ListCave = () => {
+  let { id } = useParams<any>();
+  const [cave, setCave] = useState<any>();
+
+  async function getCave() {
+    const data = await axiosInstance.get("/cave/" + id);
+    setCave(data.data);
+    console.log(cave);
+    console.log("<" + id);
+  }
+
+  useEffect(() => {
+    getCave();
+  }, []);
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>My caves</IonTitle>
+          <IonTitle>{cave?.name}</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
+        <IonButton>Add Wine</IonButton>
+        <IonButton>Edit cave</IonButton>
         <IonButton routerLink="/home">Cancel</IonButton>
       </IonContent>
     </IonPage>
